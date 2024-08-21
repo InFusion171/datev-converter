ca4d092913c9e0385f39394ed82b50895d2b03ff
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2024-08-21 14:32:44 +0200

    added styling to the window and added select_output_dirt

--- FileCreation.py
+++ FileCreation.py
@@ -4,8 +4,8 @@ from Transaction import sort_by_buchungstag
 
 class FileCreation:
     @staticmethod
-    def create_files(gui):
-        DIRECTORY_PATH = f'{os.getcwd()}/{gui.account_owner} Überweisungen'
+    def create_files(transaction_by_date: dict, account_owner: str, output_path: str):
+        DIRECTORY_PATH = f'{output_path}/{account_owner} Bankumsätze'
 
         if os.path.exists(DIRECTORY_PATH):
             messagebox.showerror('Error', f'Ordner "{DIRECTORY_PATH}" existiert bereits') 
@@ -13,9 +13,9 @@ class FileCreation:
         
         os.makedirs(DIRECTORY_PATH)
 
-        for valuta, transactions in gui.transaction_by_date.items():
+        for valuta, transactions in transaction_by_date.items():
             with open(f'{DIRECTORY_PATH}/{valuta} Sparda-Bankumsätze.txt', 'w+') as f:
                 for transaction in sort_by_buchungstag(transactions):
                     f.write(f'{transaction.create_datev_format()}\n')
 
-        messagebox.showinfo('Erfolgreich', f'Überweisungen von {gui.account_owner} in "{os.getcwd()}" erstellt')
+        messagebox.showinfo('Erfolgreich', f'Bankumsätze von {account_owner} in "{output_path}" erstellt')
--- Gui.py
+++ Gui.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
-from tkinter import Button, Entry, Label, filedialog as fd
+from tkinter import ttk
+from tkinter import filedialog as fd
 from tkinter import messagebox
 
 import chardet
@@ -14,78 +15,107 @@ class Gui:
         self.window.title(title)
         self.window.resizable(False, False)
 
-        Label(self.window, text='IBAN des gegebenen Kontos').grid(row=0)
-        Label(self.window, text='BIC des gegebenen Kontos').grid(row=1)
+        style = ttk.Style(self.window)
+        style.theme_use('clam') 
+        
+        style.configure('TLabel', font=('Arial', 12), padding=5)
+        style.configure('TButton', font=('Arial', 12), padding=5)
+        style.configure('TEntry', font=('Arial', 12), padding=5)
 
-        self.iban_input = Entry(self.window, width=30)
-        self.iban_input.grid(row=0, column=1)
+        ttk.Label(self.window, text='IBAN des gegebenen Kontos').grid(row=0, sticky='w')
+        ttk.Label(self.window, text='BIC des gegebenen Kontos').grid(row=1, sticky='w')
 
-        self.bic_input = Entry(self.window, width=30)
-        self.bic_input.grid(row=1, column=1)
+        self.iban_input = ttk.Entry(self.window, width=30)
+        self.iban_input.grid(row=0, column=1, padx=10, pady=5, sticky='ew')
 
-        self.pick_file_button = Button(self.window, text='Datei auswählen', command=self.select_file)
-        self.pick_file_button.grid(row=2, columnspan=2, pady=10, sticky='ew')
+        self.bic_input = ttk.Entry(self.window, width=30)
+        self.bic_input.grid(row=1, column=1, padx=10, pady=5, sticky='ew')
 
-        self.filename_lable = Label(self.window, text='Bitte eine Datei auswählen')
-        self.filename_lable.grid(row=3, columnspan=2, sticky='ew')
+        self.pick_file_button = ttk.Button(self.window, text='Datei auswählen', command=self.select_file)
+        self.pick_file_button.grid(row=2, columnspan=2, pady=10, padx=10, sticky='ew')
 
-        self.generate_button = Button(self.window, text='Generieren', command=self.generate)
-        self.generate_button.grid(row=4, columnspan=2, pady=(10, 0), sticky='ew')
+        self.pick_output_dir_button = ttk.Button(self.window, text='Ausgabepfad auswählen', command=self.select_output_directory)
+        self.pick_output_dir_button.grid(row=3, columnspan=2, pady=(0, 10), padx=10, sticky='ew')
+
+        # Box (Frame) für die Labels
+        self.label_frame = ttk.Frame(self.window, borderwidth=2, relief="groove")
+        self.label_frame.grid(row=4, columnspan=2, pady=10, padx=10, sticky='ew')
+
+        # Spalten des Frames konfigurieren, um die Labels zu zentrieren
+        self.label_frame.grid_columnconfigure(0, weight=1)
+
+        self.filename_label = ttk.Label(self.label_frame, text='Bitte eine Datei auswählen', anchor='center')
+        self.filename_label.grid(row=0, column=0, pady=5, padx=10, sticky='ew')
+
+        self.output_path_label = ttk.Label(self.label_frame, text='Bitte Zielordner auswählen', anchor='center')
+        self.output_path_label.grid(row=1, column=0, pady=5, padx=10, sticky='ew')
+
+        self.generate_button = ttk.Button(self.window, text='Generieren', command=self.generate)
+        self.generate_button.grid(row=5, columnspan=2, pady=10, padx=10, sticky='ew')
 
         self.account_owner = ''
         self.content = ''
+        self.output_path = ''
 
     def run(self):
         self.window.mainloop()
 
     def generate(self):
         if len(self.content) == 0:
-            messagebox.showerror("Error", "Zuerst eine Datei auswählen") 
-            return 
-        
+            messagebox.showerror("Error", "Zuerst eine Datei auswählen")
+            return
+
         if len(self.iban_input.get()) == 0:
-            messagebox.showerror("Error", "IBAN Feld ist leer") 
-            return 
+            messagebox.showerror("Error", "IBAN Feld ist leer")
+            return
 
         if len(self.bic_input.get()) == 0:
-            messagebox.showerror("Error", "BIC Feld ist leer") 
-            return 
+            messagebox.showerror("Error", "BIC Feld ist leer")
+            return
+
+        if len(self.output_path) == 0:
+            messagebox.showerror("Error", "Bitte den Zielpfad auswählen")
+            return
 
         try:
             self.account_owner, self.transaction_by_date = Transaction.parse(self.iban_input.get(), self.bic_input.get(), self.content)
-           
+
         except AttributeError:
-            messagebox.showerror("Error", "Zuerst eine Datei auswählen") 
+            messagebox.showerror("Error", "Zuerst eine Datei auswählen")
             return
         except Exception:
-            messagebox.showerror("Error", "Das Format der Datei stimmt nicht") 
+            messagebox.showerror("Error", "Das Format der Datei stimmt nicht")
             return
 
-        FileCreation.create_files(self)
-            
+        FileCreation.create_files(self.transaction_by_date, self.account_owner, self.output_path)
+
+    def select_output_directory(self):
+        path = fd.askdirectory(title='Zielpfad auswählen')
+
+        self.output_path = path
+        self.output_path_label['text'] = f'Ausgabepfad: {path}'
 
     def select_file(self):
-        path:str = fd.askopenfilename(
+        path = fd.askopenfilename(
             title='Datei auswählen',
-            initialdir=f'{os.getcwd()}',
-            filetypes=[('Spardabank Überweisungs export', '*.csv'), ('Spardabank Überweisungs export', '*.txt')])
-        
-        if len(path) == 0:
-            return
-        
-        filename = path.split('/')[len(path.split('/')) - 1]
+            filetypes=[('Spardabank Überweisungs export', '*.csv'), ('Spardabank Überweisungs export', '*.txt')]
+        )
 
-        self.filename_lable['text'] = filename
+        if not path:
+            return
+
+        filename = os.path.basename(path)
+        self.filename_label['text'] = f'Datei: {filename}'
 
         with open(path, 'rb') as file:
             raw_data = file.read()
 
-    
         result = chardet.detect(raw_data)
         encoding = result['encoding']
-    
+
         if encoding is None:
             messagebox.showerror("Error", "Das Encoding der Datei konnte nicht erkannt werden")
             return
-    
+
         self.content = raw_data.decode(encoding)
+
